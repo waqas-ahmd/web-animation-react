49aeedd03f90b0b56b5e0849212390f07c40bf75
waqas-ahmd <47054857+waqas-ahmd@users.noreply.github.com>
2020-07-19 12:46:16 +0500

    first commit

---
+++ src/Ball.js
@@ -0,0 +1,34 @@
+import React from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
+import "./App.css";
+
+function Ball(props) {
+  var indx = props.index;
+  var hue = indx * 56;
+  var speed = props.speed;
+  if(speed < 50)
+  speed = 10;
+  var delay = props.delay;
+
+  const { ref} = useWebAnimations({
+    keyframes: [
+      { transform: "translateY(0)" },
+      { transform: "translateY(-120px)" },
+    ],
+    timing: {
+      delay: delay*100, 
+      duration: speed, 
+      iterations: Infinity, 
+      direction: "alternate", 
+      easing: "cubic-bezier(0,0.5,0.5,1)",
+    }
+  });
+
+  var ballStyle = {
+    filter: "saturate(4) brightness(150%) hue-rotate(" + hue + "deg)"
+  };
+
+  return <div className="ball" style={ballStyle} ref={ref}></div>;
+}
+
+export default Ball;
